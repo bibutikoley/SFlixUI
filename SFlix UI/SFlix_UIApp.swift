d4ed589033c69a2d48be2920754b8b718741d382
Bibuti Koley <bibutikoley@outlook.com>
2021-05-08 20:27:29 +0530

    Initial Commit

---
+++ SFlix UI/SFlix_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFlix_UIApp.swift
+//  SFlix UI
+//
+//  Created by Bibuti Koley on 08/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SFlix_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
